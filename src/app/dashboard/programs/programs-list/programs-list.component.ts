e783110699cd6f865ea50a5108cc68a305271965
vikpdy <vkspandey13@gmail.com>
2022-07-18 16:20:24 +0530

    wip program

--- src/app/dashboard/programs/programs-list/programs-list.component.ts
+++ src/app/dashboard/programs/programs-list/programs-list.component.ts
@@ -16,6 +16,8 @@ import {
   AuthenticationService
 } from 'src/app/auth.service';
 import { DocumentService } from './../../../services/document.service';
+import { FormGroup, FormControl, Validators, NgForm } from "@angular/forms";
+
 @Component({
   selector: 'app-programs-list',
   templateUrl: './programs-list.component.html',
@@ -23,145 +25,23 @@ import { DocumentService } from './../../../services/document.service';
 })
 export class ProgramsListComponent implements OnInit {
 
-  collection = [{
-      
-      
-    "firstName": "01",
-    imageurl: "assets/categories/cat-1.png",
-    "userName": "LoremIpsum",
-    "email": "10"
 
-},
-{
-
-
-"firstName": "02",
-imageurl: "assets/categories/cat-2.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "03",
-imageurl: "assets/categories/cat-3.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "04",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "05",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "06",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "07",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "08",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "09",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "10",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "11",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "12",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "13",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "14",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "15",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-},{
-
-
-"firstName": "16",
-imageurl: "assets/categories/cat-1.png",
-"userName": "LoremIpsum",
-"email": "10"
-
-}
-
-];
-
+  selectedData: any
 p: number = 1;
 showAction: boolean;
+editDocumentForm:FormGroup;
+selectedIndex: any
+
 
 documents: any;
 id: any;
-constructor( private _documentService : DocumentService,private _snackbar: MatSnackBar, private router: Router, private AuthenticationService: AuthenticationService) {}
+constructor( private _documentService : DocumentService,private _snackbar: MatSnackBar, private router: Router, private AuthenticationService: AuthenticationService) {
+  this.editDocumentForm = new FormGroup ({
+    title: new FormControl (null, [Validators.required, Validators.maxLength(200)]),
+    background_image: new FormControl (null),
+
+  });
+}
 
 ngOnInit(): void {
 
@@ -181,7 +61,36 @@ this._documentService
     console.log(this.documents.payload)
   });
 
+  updateProgram(form: any, item:any) {
+    console.log(form.value)
+   console.log(item.payload.doc.id)
+      this._documentService.updateProgramDocument(item.payload.doc.id, form.value)
+      .then(
+        res => {
+          this.selectedIndex = null
+            this.showAction = false
+          this._snackbar.open("Updated Successfully", "Thanks", {
+            duration: 3000
+          });
+          this.router.navigate(['/dashboard/programs'])
+        }
+      )
+    
+  }
+
   deleteDocument = data => this._documentService.DeleteDocument(data);
+
+  editAction(index: any, data: any){
+    this.selectedIndex = index
+    console.log(this.selectedIndex)
+    this.editDocumentForm.controls['title'].setValue(data.payload.doc.data().title);
+      this.editDocumentForm.controls['background_image'].setValue(data.payload.doc.data().background_image);
+    this.showAction = true
+  }
+  cancelAction(){
+    this.selectedIndex = null
+    this.showAction = false
+  }
 
 }
 
